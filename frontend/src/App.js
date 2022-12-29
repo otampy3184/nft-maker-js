c09d7ebed75e0fa155247fc4089267ce1ce39ff4
django_freeman <o.tampodo.t.18@gmail.com>
2022-12-29 15:01:31 +0900

    debug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,23 @@ import NFTMaker from './abi/NFTMaker.json';
 import ethers from 'ethers'
 import { Web3Storage } from 'web3.storage'
 
-
 function App() {
   const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDEyZUM3OTFBREM0NGYyMmI0ODlmNEYxQTk1ODk2ODM2M0RGRUVGNzAiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjUyMzU4NjIzMTgsIm5hbWUiOiJuZnQtbWFrZXIifQ.ozxz5s4zkcGENyU9kr_pLRK1p4LBgqgGAULJRqcwxcQ";
 
   const uploadToIpfs = async (e) => {
     const client = new Web3Storage({ token: API_KEY })
     const image = e.target
-    console.log(image)
+    console.log("image data:", image)
 
     const rootCid = await client.put(image.files, {
       name: 'metadata',
       maxRetries: 3
     })
-
     const res = await client.get(rootCid) // Web3Response
     const files = await res.files() // Web3File[]
+
+    console.log("files:", await files)
+
     for (const file of files) {
       alert("file.cid:", file.cid)
     }
